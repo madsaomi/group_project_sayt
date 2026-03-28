6c740569b4919ec021ff4ab7f29b8731a0a1e14c
madsaomi13 <madsaomi13@gmail.com>
2026-03-28 07:05:11 -0700

    refactor: move app initialization to global scope in main.py

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ def create_app(config_class=Config):
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=5000)
